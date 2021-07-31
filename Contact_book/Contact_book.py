87dafedb79b8c7419d0fc19c68e6111137a66cc0
Dawid <dawidsobierajski@gmail.com>
2021-07-31 21:37:27 +0100

    began work on the contatcs book

--- Contact_book/Contact_book.py
+++ Contact_book/Contact_book.py
@@ -1,6 +1,30 @@
 import sqlite3
+from sqlite3.dbapi2 import Cursor
 
 
 conn = sqlite3.connect("Contactbook.db")
 
 cursor = conn.cursor()
+
+#cursor.execute("""CREATE TABLE contacts (
+#    first_name text,
+#    last_name text,
+#    phone_number text
+#)""")
+
+#many_contacts = [
+#                    ('Wes', 'Brown','2342563657'),
+#                    ('James','sheen','0987654321'),
+#                    ('Sam','Car','56734254434')
+#                    ]                
+#cursor.executemany("INSERT INTO contacts VALUES (?,?,?)", many_contacts)
+
+cursor.execute("SELECT * FROM contacts")
+
+print(cursor.fetchall())
+
+
+conn.commit()
+
+
+conn.close()
