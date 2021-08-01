087cd9d1ab962e9a5a486d7862c174a42754fabe
Dawid <dawidsobierajski@gmail.com>
2021-08-01 13:38:07 +0100

    created a insert contacts function a viewall function and a deleta contact function for the SQLite database but still has many bugs

--- Contact_book/Contact_book.py
+++ Contact_book/Contact_book.py
@@ -1,30 +1,61 @@
 import sqlite3
-from sqlite3.dbapi2 import Cursor
+from sqlite3.dbapi2 import Cursor, connect
 
 
 conn = sqlite3.connect("Contactbook.db")
 
 cursor = conn.cursor()
 
-#cursor.execute("""CREATE TABLE contacts (
-#    first_name text,
-#    last_name text,
-#    phone_number text
-#)""")
-
-#many_contacts = [
-#                    ('Wes', 'Brown','2342563657'),
-#                    ('James','sheen','0987654321'),
-#                    ('Sam','Car','56734254434')
-#                    ]                
-#cursor.executemany("INSERT INTO contacts VALUES (?,?,?)", many_contacts)
-
-cursor.execute("SELECT * FROM contacts")
-
-print(cursor.fetchall())
-
-
-conn.commit()
-
+cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (
+    first_name text,
+    last_name text,
+    phone_number text,
+    email_address text
+    )""")
 
 conn.close()
+
+
+def viewall():
+    print ("These are all current contacts in the database")
+    conn1 = sqlite3.connect("Contactbook.db")
+    cursor1 = conn1.cursor()
+    cursor1.execute("SELECT * FROM contacts")
+    contacts = cursor1.fetchall()
+    for item in contacts:
+        print(item[0])
+    conn1.commit
+    conn1.close
+
+
+def insertcontact():
+    conn1 = sqlite3.connect("Contactbook.db")
+    cursor1 = conn1.cursor()
+    f_name = input ("Please input the first name: ")
+    l_name = input ("Please input the last name: ")
+    p_num = int(input("Please input the phone number: "))
+    email = input("Please input the email address: ")
+    insertdetails = """INSERT INTO contacts
+                    (first_name, last_name, phone_number, email_address)
+                    VALUES(?,?,?,?)"""
+    data_tuple = (f_name,l_name,p_num,email)
+    cursor1.execute(insertdetails,data_tuple)
+    conn1.commit
+    conn1.close
+
+def deletecontact():
+    conn1 = sqlite3.connect("Contactbook.db")
+    cursor1 = conn1 .cursor()
+    name_del = input("Please Input the name of the individual you would like to delete: ")
+    deletequery = "DELETE from contacts WHERE first_name = VALUES(?)"
+    cursor1.execute(deletequery,name_del)
+    conn1.commit
+    conn1.close
+    
+
+viewall()
+insertcontact()
+insertcontact()
+insertcontact()
+viewall()
+deletecontact()
