9f0da010e800c22fa0d06540213ebbe7c13b1031
Dawid <dawidsobierajski@gmail.com>
2021-08-01 18:06:18 +0100

    Working contact book with the ability to Insert new contats, update contacts, delete contacts and view all contacts

--- Contact_book/Contact_book.py
+++ Contact_book/Contact_book.py
@@ -15,6 +15,29 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS contacts (
 
 conn.close()
 
+def main():
+    print("     THE CONTACTS BOOK       ")
+    print("-----------------------------")
+    print("There are 4 things you can do \n View your contacts \n Insert New contactm \n Update existing contact \n Delete a exising contact")
+    print("use the commands View, Insert, Update, Delete, commands to view this again and stop if you want to leave the program")
+    userinput = None
+    while userinput != "stop":
+        userinput = input("What would you like to do? ")
+        if userinput == "View":
+            viewall()
+        if userinput == "Insert":
+            insertcontact()
+        if userinput == "Update":
+            updatecontact()
+        if userinput == "Delete":
+            deletecontact()
+        if userinput == "commands":
+            commands()
+        if userinput == "stop":
+            break
+
+
+
 
 def viewall():
     print ("These are all current contacts in the database")
@@ -23,9 +46,9 @@ def viewall():
     cursor1.execute("SELECT * FROM contacts")
     contacts = cursor1.fetchall()
     for item in contacts:
-        print(item[0])
-    conn1.commit
-    conn1.close
+        print(item)
+    conn1.commit()
+    conn1.close()
 
 
 def insertcontact():
@@ -33,29 +56,34 @@ def insertcontact():
     cursor1 = conn1.cursor()
     f_name = input ("Please input the first name: ")
     l_name = input ("Please input the last name: ")
-    p_num = int(input("Please input the phone number: "))
+    p_num = str(input("Please input the phone number: "))
     email = input("Please input the email address: ")
-    insertdetails = """INSERT INTO contacts
-                    (first_name, last_name, phone_number, email_address)
-                    VALUES(?,?,?,?)"""
-    data_tuple = (f_name,l_name,p_num,email)
-    cursor1.execute(insertdetails,data_tuple)
-    conn1.commit
-    conn1.close
+    cursor1.execute("INSERT INTO contacts (first_name, last_name, phone_number, email_address) VALUES(?,?,?,?)", (f_name, l_name, p_num, email))
+    conn1.commit()
+    conn1.close()
 
 def deletecontact():
     conn1 = sqlite3.connect("Contactbook.db")
-    cursor1 = conn1 .cursor()
+    cursor1 = conn1.cursor()
     name_del = input("Please Input the name of the individual you would like to delete: ")
-    deletequery = "DELETE from contacts WHERE first_name = VALUES(?)"
-    cursor1.execute(deletequery,name_del)
-    conn1.commit
-    conn1.close
-    
+    cursor1.execute("DELETE from contacts WHERE first_name LIKE ?", (name_del,))
+    conn1.commit()
+    conn1.close()
 
-viewall()
-insertcontact()
-insertcontact()
-insertcontact()
-viewall()
-deletecontact()
+def updatecontact():
+    conn1 = sqlite3.connect("Contactbook.db")
+    cursor1 = conn1.cursor()
+    a = input("Which contact would you like to modify?: ")
+    b = input("Which part of the contact would you like to modify?: ")
+    c = input("What would you like to change the value too?: ")
+    info_tuple = (c,a)
+    update_query = """UPDATE contacts SET {} = ? WHERE first_name LIKE ?"""
+    fomated_query = update_query.format(b)
+    cursor1.execute(fomated_query, info_tuple)
+    conn1.commit()
+    conn1.close()
+
+def commands():
+    print("use the commands View, Insert, Update, Delete, commands to view this again and stop if you want to leave the program")
+    
+main()
